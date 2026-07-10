d41f1754e95281434c6fd613e648f19f107e6d53
yuzyayvri <laoyuanalistaire@gmail.com>
2026-07-11 00:22:49 +0800

    Set default NNUEPath to nnue.bin, remove stale buggy weight files

--- src/core/engine_options.hpp
+++ src/core/engine_options.hpp
@@ -6,7 +6,7 @@ struct EngineOptions {
     int threads = 1;
     int hashMB = 128;
     std::string bookPath = "book.bin";
-    std::string nnuePath = "";
+    std::string nnuePath = "nnue.bin";
     int moveOverhead = 10;
     bool ponder = false;
 };
